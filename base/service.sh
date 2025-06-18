c87600223fbcddd70f12e48f7beda2a6cff69a53
Droid-MAX <Droid-MAX@users.noreply.github.com>
2025-06-19 05:58:11 +0800

    feat: Parsing of FakeHTTP -a parameters for the version 0.9.15

--- base/service.sh
+++ base/service.sh
@@ -34,7 +34,7 @@ load_config
 
 set -- -d -z
 
-[ -n "${interface+x}" ] && set -- "$@" "-i" "$interface"
+[ -n "${interface+x}" ] && { [ "$interface" == "all" ] && set -- "$@" -a || set -- "$@" -i "$interface"; }
 [ -n "${hostname+x}" ] && set -- "$@" "-h" "$hostname"
 [ -n "${mark+x}" ] && set -- "$@" "-m" "$mark"
 [ -n "${mask+x}" ] && set -- "$@" "-x" "$mask"
